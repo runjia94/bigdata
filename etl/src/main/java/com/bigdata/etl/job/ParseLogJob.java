c2a698f7cab3481e35e692e6b12ca2f913486d22
runjia94 <runjia94@icloud.com>
2018-12-22 23:50:45 -0700

    for general case

--- etl/src/main/java/com/bigdata/etl/job/ParseLogJob.java
+++ etl/src/main/java/com/bigdata/etl/job/ParseLogJob.java
@@ -2,7 +2,8 @@ package com.bigdata.etl.job;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.bigdata.etl.mr.LogBeanWritable;
+import com.bigdata.etl.mr.LogFieldWritable;
+import com.bigdata.etl.mr.LogGenericWritable;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
@@ -22,25 +23,24 @@ import org.apache.hadoop.util.ToolRunner;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Map;
 
 public class ParseLogJob extends Configured implements Tool {
-    public static LogBeanWritable parseLog(String row) throws ParseException {
+
+    public static LogGenericWritable parseLog(String row) throws ParseException {
         String[] logPart = StringUtils.split(row,"\u1111");
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         long timeTag = dateFormat.parse(logPart[0]).getTime();
         String activeName = logPart[1];
         JSONObject bizData = JSON.parseObject(logPart[2]);
 
-        LogBeanWritable logData = new LogBeanWritable();
-        logData.setActiveName((activeName));
-        logData.setTimeTag(timeTag);
-        logData.setDeviceID(bizData.getString("device_id"));
-        logData.setIp(bizData.getString("ip"));
-        logData.setOrderID(bizData.getString("order_id"));
-        logData.setProductID(bizData.getString("product_id"));
-        logData.setReqUrl(bizData.getString("req_url"));
-        logData.setSessionID(bizData.getString("session_id"));
-        logData.setUserID(bizData.getString("user_id"));
+        LogGenericWritable logData = new LogWritable();
+        logData.put("time_tag", new LogFieldWritable(timeTag));
+        logData.put("active_name", new LogFieldWritable(activeName));
+
+        for(Map.Entry<String, Object> entry: bizData.entrySet()){
+            logData.put(entry.getKey(), new LogFieldWritable(entry.getValue()));
+        }
 
 
         return logData;
@@ -49,12 +49,22 @@ public class ParseLogJob extends Configured implements Tool {
 
     }
 
+    public static class LogWritable extends LogGenericWritable{
+
+        @Override
+        protected String[] getFieldName() {
+            return new String[]{"active_name", "session_id", "time_tag", "ip", "device_id", "req_url", "user_id", "product_id", "order_id"};
 
 
-    public static class LogMapper extends Mapper<LongWritable, Text, LongWritable, LogBeanWritable>{
+        }
+    }
+
+
+
+    public static class LogMapper extends Mapper<LongWritable, Text, LongWritable, LogGenericWritable>{
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
             try {
-                LogBeanWritable parsedLog = parseLog(value.toString());
+                LogGenericWritable parsedLog = parseLog(value.toString());
                 context.write(key,parsedLog);
             } catch (ParseException e) {
                 e.printStackTrace();
@@ -62,9 +72,9 @@ public class ParseLogJob extends Configured implements Tool {
         }
     }
 
-    public static class LogReducer extends Reducer<LongWritable, LogBeanWritable, NullWritable, Text>{
-        public void reduce(LongWritable key, Iterable<LogBeanWritable> values, Context context) throws IOException, InterruptedException{
-            for(LogBeanWritable v : values){
+    public static class LogReducer extends Reducer<LongWritable, LogGenericWritable, NullWritable, Text>{
+        public void reduce(LongWritable key, Iterable<LogGenericWritable> values, Context context) throws IOException, InterruptedException{
+            for(LogGenericWritable v : values){
                 context.write(null, new Text(v.asJsonString()));
             }
 
@@ -81,7 +91,7 @@ public class ParseLogJob extends Configured implements Tool {
         job.setMapperClass(LogMapper.class);
         job.setReducerClass(LogReducer.class);
         job.setMapOutputKeyClass(LongWritable.class);
-        job.setMapOutputValueClass(LogBeanWritable.class);
+        job.setMapOutputValueClass(LogWritable.class);
         job.setOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
